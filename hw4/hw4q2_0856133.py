5980234c28ca855a7ed43d8ef8dc829f6563632d
jimmy <jimmy@jimmyde-MacBook-Air.local>
2019-11-18 15:23:08 +0800

    using 30 images per epoch, and run 20 epoch, the result is quite significant, with 3284951(it's a vertical bar now, i think it would be 7, if given more image or more epoch)1(definitely 1, because of the bottom horizontal line)60

--- hw4/hw4q2_0856133.py
+++ hw4/hw4q2_0856133.py
@@ -8,7 +8,7 @@ def loadFile():
     test_image, test_label = mlxtend.data.loadlocal_mnist(
         images_path="./t10k-images-idx3-ubyte",
         labels_path="./t10k-labels-idx1-ubyte")
-    return train_image[:10000, ], train_label[:10000], test_image[:100, ], test_label[:100]
+    return train_image[:30, ], train_label[:30], test_image[:100, ], test_label[:100]
 
 def binning(image):
     binning_image = np.zeros((len(image), 28, 28))
@@ -38,6 +38,12 @@ test_image = binning(test_image)
 #         print(train_image[0][i][j], " ", end='')
 #     print("\n")
 full_train_image = train_image
+#print(full_train_image)
+# for i in range(len(full_train_image)):
+#     for j in range(28):
+#         for k in range(28):
+#             print(full_train_image[i][j][k], " ", end='')
+#         print("\n")
 
 # parameter:
     # w[10][60000]
@@ -64,7 +70,7 @@ for i in range(len(train_image)):
     w[7][i] = 6265/60000
     w[8][i] = 5851/60000
     w[9][i] = 5949/60000
-print(w)
+#print(w)
 
 # for i in range(10):
 #     lambd[i] = 0.1
@@ -84,7 +90,7 @@ lambd[8] = 5851
 lambd[9] = 5949
 for i in range(10):
     lambd[i] /= 60000
-    print("lambd[%d] :" % i, lambd[i])
+    #print("lambd[%d] :" % i, lambd[i])
 
 
 for i in range(10):
@@ -92,79 +98,101 @@ for i in range(10):
         for k in range(28):
             P[i][j][k] = 0.05
 
-print("=============E step===============")
-# E-step
-for i in range(len(train_image)):
-    print("Picture[%d] : \n" % i)
-    # for s in range(28):
-    #     for t in range(28):
-    #         print(train_image[i][s][t], " ", end='')
-    #     print("\n")
-    denominator = 0
-    for category in range(10):
-        numerator = lambd[category]
-        for j in range(28):
-            for k in range(28):
-                numerator *= np.power(P[category][j][k], train_image[i][j][k]) * np.power((1-P[category][j][k]), (1-train_image[i][j][k]))
-        print(numerator)
-        w[category][i] = numerator
-        denominator += numerator
-    for category in range(10):
-        w[category][i] /= denominator
-        print(w[category][i])
-
-print("=============M step===============")
-print("=============update lambd===========")
-# M-step
-# update lambd
-lambdMLE = np.zeros(10)
-for category in range(9):
-    lambdMLE[category] = lambd[category] + lambd[9]
-#     print(lambdMLE[category])
-# print(lambdMLE[9])
-    numerator = 0
-    denominator = 0
-    for i in range(len(train_image)):
-    #     print("Picture[%d] : \n" % i)
-    #     for s in range(28):
-    #         for t in range(28):
-    #             print(train_image[i][s][t], " ", end='')
+for epoch in range(20):
+    print("===============================epoch[%d]==================================" % epoch)
+    #train_image = full_train_image[300*(epoch):300*(epoch+1), :, :]
+    # for i in range(len(train_image)):
+    #     for j in range(28):
+    #         for k in range(28):
+    #             print(train_image[i][j][k], " ", end='')
     #         print("\n")
-        numerator += w[category][i]
-        denominator += w[category][i] + w[9][i]
-    lambdMLE[category] *= numerator/denominator
-    print("partial_lambd[%d] : numerator, denominator, lambdMLE[%d]" % (category, category), numerator, denominator, lambdMLE[category])
-lambdMLE[9] = 1-lambdMLE[0]-lambdMLE[1]-lambdMLE[2]-lambdMLE[3]-lambdMLE[4]-lambdMLE[5]-lambdMLE[6]-lambdMLE[7]-lambdMLE[8]
-print("partial_lambd[9] :", lambdMLE[9])
-lambd = lambdMLE
+    #     print("\n")
+    #print("=============E step===============")
+    # E-step
+    for i in range(len(train_image)):
+        #print("Picture[%d] : \n" % i)
+        # for s in range(28):
+        #     for t in range(28):
+        #         print(train_image[i][s][t], " ", end='')
+        #     print("\n")
+        denominator = 0
+        for category in range(10):
+            numerator = lambd[category]
+            for j in range(28):
+                for k in range(28):
+                    numerator *= np.maximum(np.power(P[category][j][k], train_image[i][j][k]), 0.05) \
+                                 * np.maximum(np.power((1-P[category][j][k]), (1-train_image[i][j][k])), 0.05)
+                    #print("numerator:", numerator)
+            w[category][i] = numerator
+            denominator += numerator
+            #print("denominator", denominator)
+        for category in range(10):
+            w[category][i] /= denominator
+            #print(w[category][i])
+    #print(w)
+    #print("=============M step===============")
+    #print("=============update lambd===========")
+    # M-step
+    # update lambd
+    lambdMLE = np.zeros(10)
+    for category in range(9):
+        lambdMLE[category] = lambd[category] + lambd[9]
+    #     print(lambdMLE[category])
+    # print(lambdMLE[9])
+        numerator = 0
+        denominator = 0
+        for i in range(len(train_image)):
+        #     print("Picture[%d] : \n" % i)
+        #     for s in range(28):
+        #         for t in range(28):
+        #             print(train_image[i][s][t], " ", end='')
+        #         print("\n")
+            numerator += w[category][i]
+            denominator += w[category][i] + w[9][i]
+        lambdMLE[category] *= numerator/denominator
+        #print("partial_lambd[%d] : numerator, denominator, lambdMLE[%d]" % (category, category), numerator, denominator, lambdMLE[category])
+    lambdMLE[9] = 1-lambdMLE[0]-lambdMLE[1]-lambdMLE[2]-lambdMLE[3]-lambdMLE[4]-lambdMLE[5]-lambdMLE[6]-lambdMLE[7]-lambdMLE[8]
+    #print("partial_lambd[9] :", lambdMLE[9])
+    lambd = lambdMLE
 
-print("==============update P================")
-# update P
-for category in range(10):
-    print("category[%d] : " % category)
-    for row in range(28):
-        for col in range(28):
-            numerator = 0
-            denominator = 0
-            for i in range(len(train_image)):
-                numerator += w[category][i]*train_image[i][row][col]
-                denominator += w[category][i]
-            #print(numerator, denominator)
-            P[category][row][col] = numerator / denominator
-            print(P[category][row][col], " ", end='')
-        print("\n")
+    # for i in range(10):
+    #     for j in range(28):
+    #         for k in range(28):
+    #             print(P[i][j][k], " ", end='')
+    #         print("\n")
+    #print("==============update P================")
+    # update P
+    for category in range(10):
+        #print("category[%d] : " % category)
+        for row in range(28):
+            for col in range(28):
+                numerator = 0
+                denominator = 0
+                for i in range(len(train_image)):
+                    #print(w[category][i], train_image[i][row][col])
+                    numerator += w[category][i]*train_image[i][row][col]
+                    denominator += w[category][i]
+                #print(numerator, denominator)
+                P[category][row][col] = numerator / denominator
+                #print(P[category][row][col], " ", end='')
+            #print("\n")
+#     for i in range(10):
+#         for j in range(28):
+#             for k in range(28):
+#                 print(P[i][j][k], " ", end='')
+#             print("\n")
 
-print("==================imagination picture======================")
-# show result
-for category in range(10):
-    print("class %d:" % category)
-    for i in range(28):
-        for j in range(28):
-            if P[category][i][j] >= 0.5:
-                print(1, end='')
-            else:
-                print(0, end='')
-        print("\n")
+    print("==================imagination picture======================")
+    # show result
+    for category in range(10):
+        print("class %d:" % category)
+        for i in range(28):
+            for j in range(28):
+                if P[category][i][j] >= 0.5:
+                    print(1, end='')
+                else:
+                    print(0, end='')
+            print("\n")
 
 # if __name__ == "__main__":
 #     EM()
